6b72a2ee18a2732a1f8efac3161756f7e23d9884
MartaBetanPhil <39142543+MartaBetanPhil@users.noreply.github.com>
2018-06-03 09:46:35 +0800

    Update excise01.py

--- excise01.py
+++ excise01.py
@@ -8,7 +8,7 @@ Created on Sun Jun  3 09:37:50 2018
 ###作业
 print("欢迎使用天气app")
 city_pinyin=input("请输入城市拼音:")
-address='http://api.openweathermap.org/data/2.5/weather?q=meishan&mode=json&units=metric&lang=zh_cn&APPID=6a67ed641c0fda8b69715c43518b6996'
+address='http://api.openweathermap.org/data/2.5/weather?q={}&mode=json&units=metric&lang=zh_cn&APPID=6a67ed641c0fda8b69715c43518b6996'
 info=r.urlopen(address.format(city_pinyin)).read().decode('utf-8','ignore')  
 data=json.loads(info)
 print('今天天气是：'+data['weather'][0]['description']+',温度是：'+str(data['main']['temp'])+',气压是：'+str(data['main']['pressure']))
